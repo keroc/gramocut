34090d2f0f5b7d85e1a7cc4014f3d808bcdba0fd
Kévin Rocheteau <kevroc@mailo.com>
2024-06-04 22:25:30 +0200

    Finalize waveform widget
    - Add zoom command
    - Add drag command (right click)
    - Add cursor definition (left click)

--- gramocut/gramocut.py
+++ gramocut/gramocut.py
@@ -5,7 +5,7 @@ Gramocut entry point and main application
 '''
 
 import customtkinter as ctk
-from pydub import AudioSegment, effects
+from pydub import AudioSegment
 
 class VinylWidget(ctk.CTkCanvas):
     '''
@@ -51,40 +51,68 @@ class VinylWidget(ctk.CTkCanvas):
 class Waveform(ctk.CTkFrame):
     def __init__(self, root, height=100):
         self.height = height
-        self.wave = None
+        self.wave = []
 
         self.view_start = 0
         self.view_end = 0
         self.cursor = 0
 
+        # Initialize internal widgets
         super().__init__(root)
+        self.label_frame = ctk.CTkFrame(self)
+        self.start_label = ctk.CTkLabel(self.label_frame, text='start', text_color='#A0A0A0')
+        self.cursor_label = ctk.CTkLabel(self.label_frame, text='cursor', text_color='#A0A0A0')
+        self.end_label = ctk.CTkLabel(self.label_frame, text='end', text_color='#A0A0A0')
+        self.start_label.pack(side='left')
+        self.end_label.pack(side='right')
+        self.cursor_label.pack(fill='x')
         self.canvas = ctk.CTkCanvas(self, height=self.height, bg='#404040')
-        self.scroll = ctk.CTkScrollbar(self, orientation='horizontal')
+        self.label_frame.pack(fill='x')
         self.canvas.pack(fill='x')
-        self.scroll.pack(fill='x')
 
         self.update()
 
-        # Update on each size change
+        # Bind events
         self.canvas.bind('<Configure>', self.update)
+        self.canvas.bind('<Button-1>', self.click_callback)
+        self.canvas.bind('<ButtonPress-3>', self.drag_start_callback)
+        self.canvas.bind('<B3-Motion>', self.drag_callback)
+    
+    def ms_format(self, ms):
+        ms_part = ms % 1000
+        s_part = int(ms/1000)%60
+        min_part = int(ms/60000)
+        return '{:0d}m:{:02d}s:{:03d}'.format(min_part, s_part, ms_part)
 
     def update(self, event=None):
         self.canvas.delete('all')
 
-        if self.wave is None:
+        # Update time label
+        self.start_label.configure(text=self.ms_format(self.view_start))
+        self.end_label.configure(text=self.ms_format(self.view_end))
+        self.cursor_label.configure(text='{} / {}'.format(self.ms_format(self.cursor), self.ms_format(len(self.wave))))
+
+        if len(self.wave) == 0:
             # If no segment loaded, stop here
             return
         
+        # Compute metrics
         width = self.canvas.winfo_width()
         height = self.canvas.winfo_height()
         ms_per_pix = int((self.view_end - self.view_start) / width)
         
+        # Draw waveform
         for pix in range(0,width):
             pix_start_ms = self.view_start + pix*ms_per_pix
             pix_end_ms = self.view_start + (pix+1)*ms_per_pix
             vol = max(self.wave[pix_start_ms:pix_end_ms])*height
             space = int((height-vol)/2)
             self.canvas.create_line(pix, space, pix, space+vol, fill='white')
+        
+        # Draw cursor if inside the canvas
+        if self.view_start <= self.cursor and self.cursor < self.view_end:
+            x = int((self.cursor - self.view_start) / ms_per_pix)
+            self.canvas.create_line(x, 0, x, height, fill='red')
         
         
     
@@ -100,6 +128,74 @@ class Waveform(ctk.CTkFrame):
         self.cursor = 0
 
         self.update()
+
+    def apply_zoom(self, zoom):
+        '''
+        Apply a zoom factor, if zoom < 1 it's a zoom in, if zoom > 1 it's a zoom out
+        '''
+        view_width = self.view_end - self.view_start
+        new_width = max(zoom * view_width, 10000)
+        offset = int((view_width - new_width)/2)
+        
+        self.view_start += offset
+        if self.view_start < 0:
+            self.view_start = 0
+        
+        self.view_end -= offset
+        if self.view_end > len(self.wave):
+            self.view_end = len(self.wave)
+
+        self.update()
+    
+    def zoom_in(self):
+        '''
+        Zoom in on the waveform
+        '''
+        self.apply_zoom(0.8)
+    
+    def zoom_out(self):
+        '''
+        Zoom out on the waveform
+        '''
+        self.apply_zoom(1.2)
+    
+    def reset_zoom(self):
+        '''
+        Reset the zoom to display the whole waveform
+        '''
+        self.view_start = 0
+        self.view_end = len(self.wave)
+        self.update()
+         
+    def click_callback(self, event):
+        # Update cursor position
+        width = self.canvas.winfo_width()
+        self.cursor = int((event.x / width) * (self.view_end - self.view_start)) + self.view_start
+        self.update()
+    
+    def drag_start_callback(self, event):
+        self.last_drag_x = event.x
+    
+    def drag_callback(self, event):
+        ms_per_pix = (self.view_end - self.view_start) / self.canvas.winfo_width()
+        ms_offset = int((self.last_drag_x - event.x) * ms_per_pix)
+
+        if ms_offset < 0:
+            # Limit drag to the left
+            if (ms_offset * -1) > self.view_start:
+                ms_offset = -1 * self.view_start
+        else :
+            # Limit drag to the right
+            if self.view_end + ms_offset > len(self.wave) :
+                ms_offset = len(self.wave) - self.view_end
+
+        # Drag the view
+        self.view_start += ms_offset
+        self.view_end += ms_offset
+        self.update()
+
+        self.last_drag_x = event.x
+        
 
 
 class SourceFrame(ctk.CTkFrame):
@@ -126,8 +222,12 @@ class SourceFrame(ctk.CTkFrame):
         self.waveform.grid(row=1, column=1, padx=5, pady=5, sticky='WE')
 
         self.tool_frame = ctk.CTkFrame(self)
-        self.play_button = ctk.CTkButton(self.tool_frame, text="play")
-        self.play_button.pack(side='left')
+        self.zoom_in_button = ctk.CTkButton(self.tool_frame, text="Z In", command=self.waveform.zoom_in)
+        self.zoom_out_button = ctk.CTkButton(self.tool_frame, text="Z Out", command=self.waveform.zoom_out)
+        self.zoom_reset_button = ctk.CTkButton(self.tool_frame, text="Z Reset", command=self.waveform.reset_zoom)
+        self.zoom_in_button.pack(side='left')
+        self.zoom_out_button.pack(side='left')
+        self.zoom_reset_button.pack(side='left')
         self.tool_frame.grid(row=2, column=1, padx=5, pady=5, sticky='WE')
 
         self.columnconfigure(1, weight=1)
