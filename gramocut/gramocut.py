38255a85da84c968f22a4b8851b0de03e1d14620
Kévin Rocheteau <kevroc@mailo.com>
2024-06-17 16:20:16 +0200

    Add tracks creation and deletion
    - Add tracks vizualization on the waveform
    - Add Track widgets in the bottopm part of the screen
    - Add requirements.txt file for pip

--- gramocut/gramocut.py
+++ gramocut/gramocut.py
@@ -5,7 +5,23 @@ Gramocut entry point and main application
 '''
 
 import customtkinter as ctk
+import tkinter as tk
 from pydub import AudioSegment
+import itertools
+
+TRACK_COLORS = ['blue',
+                'red',
+                'green',
+                'pink']
+
+def get_track_color(id):
+    return TRACK_COLORS[id % len(TRACK_COLORS)]
+
+def ms_format(ms):
+        ms_part = ms % 1000
+        s_part = int(ms/1000)%60
+        min_part = int(ms/60000)
+        return '{:0d}m:{:02d}s:{:03d}'.format(min_part, s_part, ms_part)
 
 class VinylWidget(ctk.CTkCanvas):
     '''
@@ -29,6 +45,14 @@ class VinylWidget(ctk.CTkCanvas):
     
     def convert_to_radius(self, percent):
         return int((1.0-percent) * (self.diskRadius - self.tagRadius) + self.tagRadius)
+
+    def change_track_time(self, start=None, end=None):
+        if start is not None:
+            self.start = start
+        if end is not None:
+            self.end = end
+        
+        self.update()
 
     def update(self):
         self.delete('all')
@@ -88,9 +112,9 @@ class Waveform(ctk.CTkFrame):
         self.canvas.delete('all')
 
         # Update time label
-        self.start_label.configure(text=self.ms_format(self.view_start))
-        self.end_label.configure(text=self.ms_format(self.view_end))
-        self.cursor_label.configure(text='{} / {}'.format(self.ms_format(self.cursor), self.ms_format(len(self.wave))))
+        self.start_label.configure(text=ms_format(self.view_start))
+        self.end_label.configure(text=ms_format(self.view_end))
+        self.cursor_label.configure(text='{} / {}'.format(ms_format(self.cursor), ms_format(len(self.wave))))
 
         if len(self.wave) == 0:
             # If no segment loaded, stop here
@@ -100,6 +124,22 @@ class Waveform(ctk.CTkFrame):
         width = self.canvas.winfo_width()
         height = self.canvas.winfo_height()
         ms_per_pix = int((self.view_end - self.view_start) / width)
+        
+        # Draw tracks if inside the canvas
+        for id, track in enumerate(self.winfo_toplevel().tracks_list) :
+            ms_start = max(track.start, self.view_start)
+            if ms_start > self.view_end :
+                # Track starts after the view, pass
+                continue
+
+            ms_end = min(track.end, self.view_end)
+            if ms_end < self.view_start :
+                # Track ends before the view, pass
+                continue
+
+            x_start = int((ms_start - self.view_start) / ms_per_pix)
+            x_end = int((ms_end - self.view_start) / ms_per_pix)
+            self.canvas.create_rectangle(x_start,0,x_end,height,fill=get_track_color(id),outline='')
         
         # Draw waveform
         for pix in range(0,width):
@@ -238,18 +278,152 @@ class SourceFrame(ctk.CTkFrame):
             self.root.source_segment = AudioSegment.from_file(filename)
             self.waveform.set_audio(self.root.source_segment)
             self.source_label.configure(text=filename)
+        
+            self.winfo_toplevel().tracks_list.clear()
+            self.winfo_toplevel().update()
+    
+    def get_new_track(self):
+        return Track(self.waveform.cursor, min(self.waveform.cursor+30000, len(self.waveform.wave)))
 
+class TrackWidget(ctk.CTkFrame):
+    '''
+    Visual representation of a track with all associated info and commands
+    '''
+    def __init__(self, master, track, id):
+        super().__init__(master)
+
+        # Vinyl Widget
+        self.vinyl = VinylWidget(self)
+        self.vinyl.grid(row=0, column=0, rowspan=2, padx=5, pady=5)
+
+        # Information Frame
+        self.info_frame = ctk.CTkFrame(self)
+        self.number_input = ctk.CTkEntry(self.info_frame, placeholder_text='<N°>')
+        self.title_input = ctk.CTkEntry(self.info_frame, placeholder_text='<Title>')
+        self.artist_input = ctk.CTkEntry(self.info_frame, placeholder_text='<Artist>')
+        self.duration_text = tk.StringVar(self.info_frame, value='duration: 0s')
+        self.duration_label = ctk.CTkLabel(self.info_frame, textvariable=self.duration_text)
+        self.number_input.pack(padx=5)
+        self.title_input.pack(padx=5)
+        self.artist_input.pack(padx=5)
+        self.duration_label.pack(padx=5)
+        self.info_frame.grid(row=0, column=1, padx=5, pady=5, sticky='NSEW')
+
+        # Command Frame
+        self.command_frame = ctk.CTkFrame(self)
+        self.zoom_button = ctk.CTkButton(self.command_frame, text='Zoom')
+        self.start_button = ctk.CTkButton(self.command_frame, text='Start')
+        self.stop_button = ctk.CTkButton(self.command_frame, text='Stop')
+        self.delete_button = ctk.CTkButton(self.command_frame, text='Delete', command=self.delete_callback)
+        self.zoom_button.pack(side='left')
+        self.start_button.pack(side='left')
+        self.stop_button.pack(side='left')
+        self.delete_button.pack(side='right')
+        self.command_frame.grid(row=1, column=1, padx=5, pady=5, sticky='EW')
+
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+
+        self.update(track, id)
+    
+    def delete_callback(self):
+        # Delete the track from the list
+        self.winfo_toplevel().tracks_list.pop(self.id)
+        self.winfo_toplevel().update()
+    
+    def update(self, track, id):
+        # Save id for commands
+        self.id = id
+
+        # Update vinyl widget
+        self.vinyl.trackColor = get_track_color(id)
+        total_time = len(self.winfo_toplevel().source_segment)
+        self.vinyl.change_track_time(track.start/total_time, track.end/total_time)
+
+class TracksFrame(ctk.CTkFrame):
+    '''
+    Frame to handle the created tracks
+    '''
+    def __init__(self, root):
+        super().__init__(root)
+
+        # Toolbar
+        self.tool_frame = ctk.CTkFrame(self)
+        self.new_track_button = ctk.CTkButton(self.tool_frame, text='+ Create new track', command=self.create_track_callback)
+        self.new_track_button.pack(side='left', padx=5)
+        self.tool_frame.pack(fill='x')
+
+        # Tracklist
+        self.tracks_list_frame = ctk.CTkScrollableFrame(self)
+        self.tracks_list_frame.pack(fill='both', expand=True)
+    
+    def create_track_callback(self):
+        # Check if everything is ready for a track
+        if self.master.source_segment is None:
+            return
+        
+        self.master.tracks_list.append(self.master.srcFrame.get_new_track())
+
+        # Update the App
+        self.winfo_toplevel().update()
+    
+    def update(self):
+        for i, (track, widget) in enumerate(itertools.zip_longest(self.master.tracks_list, self.tracks_list_frame.winfo_children())):
+            if track is None:
+                # More widget than tracks, remove it
+                widget.destroy()
+            
+            elif widget is None:
+                # we need a new widget
+                new_widget = TrackWidget(self.tracks_list_frame, track, i)
+                new_widget.pack(fill='x', padx=5, pady=5)
+            
+            else:
+                # Just update the widget
+                widget.update(track, i)
+
+class Track():
+    '''
+    Representation  of a tracks with its metadata
+    '''
+
+    def __init__(self, start=0, end=0):
+        self.start = start
+        self.end = end
 
 # Application class (root widget)
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
 
+        self.title('Gramocut')
+
         self.source_segment = None
+        self.tracks_list = []
 
         # Add the source frame
         self.srcFrame = SourceFrame(self)
-        self.srcFrame.pack(padx=5, pady=5, fill="x")
+        self.srcFrame.grid(row=0, column=0, columnspan=2, sticky='NSEW', padx=5, pady=5)
+
+        # Add the tracks frame
+        self.tracksFrame = TracksFrame(self)
+        self.tracksFrame.grid(row=1, column=0, sticky='NSEW', padx=5, pady=5)
+
+        # Add the export frame
+        self.exportFrame = ctk.CTkFrame(self)
+        self.exportFrame.grid(row=1, column=1, sticky='NSEW', padx=5, pady=5)
+
+        self.columnconfigure(0, weight=3)
+        self.columnconfigure(1, weight=1)
+        self.rowconfigure(1, weight=1)
+    
+    def update(self):
+        # Update every widget
+        self.srcFrame.waveform.update()
+        self.tracksFrame.update()
+
+
+
 
 
 # Entry point
